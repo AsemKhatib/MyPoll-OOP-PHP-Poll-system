f929eca7737bc9f35b1cb155368b6bc21b321885
AsemKhatib <ebncana@hotmail.com>
2015-12-23 01:29:22 +0000

    add comment test

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,7 @@ use MyPoll\Classes\AdminIndex;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../includes/config.php';
 
+// Start the session
 session_start();
 
 $twigAdmin->addGlobal('session', $_SESSION);
