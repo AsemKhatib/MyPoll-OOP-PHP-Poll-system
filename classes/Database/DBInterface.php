455db55ad633315e9e52764bc8bc9e8fd67859c3
Asem Khatib <khatib@DELL-E5570-194.gvl.local>
2016-11-05 01:13:36 +0100

    second try to implement database interface

--- classes/Database/DBInterface.php
+++ classes/Database/DBInterface.php
@@ -10,7 +10,7 @@ interface DBInterface
      * @param string $db_dsn
      * @param string $db_user
      * @param string $db_pass
-     * @param mixed $db_options
+     * @param mixed  $db_options
      */
     public function __construct($db_dsn, $db_user, $db_pass, $db_options = '');
 
@@ -20,22 +20,65 @@ interface DBInterface
     public function setup();
 
     /**
-     * @return mixed
-     */
-    public function getConnection();
-
-    /**
-     * @param string $dbName
+     * @param string $dbName   type of bean we are looking for
+     * @param string $addSQL   additional SQL snippet
+     * @param array  $bindings parameters to bind to SQL
      *
      * @return int
      */
-    public function count($dbName);
+    public function count($dbName, $addSQL = '', $bindings = array());
 
     /**
-     * @param string|array $stringOrArray
+     * @param AddColumnType $type
+     */
+    public function addColumn(AddColumnType $type);
+
+    /**
+     * @param StoreType $type
+     */
+    public function store(StoreType $type);
+
+    /**
+     * Convenience function to execute Queries directly.
+     * Executes SQL.
+     *
+     * @param string $sql      SQL query to execute
+     * @param array  $bindings a list of values to be bound to query parameters
+     *
+     * @return array
+     */
+    public function getAll($sql, $bindings);
+
+
+    public function getRow();
+
+    /**
+     * @param string $dbName
+     * @param int    $id
      *
      * @return mixed
      */
-    public function addColumn($stringOrArray);
+    public function getById($dbName, $id);
 
+    /**
+     * @param mixed $column
+     * @param int   $id
+     */
+    public function delete($column, $id = null);
+
+    /**
+     * @param array $columns
+     */
+    public function deleteAll($columns);
+
+    /**
+     * @param string $type
+     * @param string $sql
+     * @param array  $bindings
+     *
+     * @return array
+     */
+    public function find($type, $sql = null, $bindings = array());
+
+    public function findOne();
 }
--- classes/Database/RedBeanDB.php
+++ classes/Database/RedBeanDB.php
@@ -2,8 +2,10 @@
 
 namespace MyPoll\Classes\Database;
 
+use phpDocumentor\Reflection\Types\Array_;
 use RedBeanPHP\Facade;
 use RedBeanPHP\ToolBox;
+use RedBeanPHP\OODBBean;
 
 class RedBeanDB implements DBInterface
 {
@@ -18,9 +20,6 @@ class RedBeanDB implements DBInterface
 
     /** @var array */
     protected $db_options;
-
-    /** @var  ToolBox */
-    protected $toolBox;
 
     /**
      * RedBeanDB constructor.
@@ -39,40 +38,93 @@ class RedBeanDB implements DBInterface
     }
 
     /**
-     * @return mixed
+     * @return void
      */
     public function setup()
     {
         Facade::setup($this->db_dsn, $this->db_user, $this->db_pass);
-        $this->toolBox = Facade::getToolBox();
     }
 
     /**
-     * @return ToolBox
+     * @param string $dbName   type of bean we are looking for
+     * @param string $addSQL   additional SQL snippet
+     * @param array  $bindings parameters to bind to SQL
+     *
+     * @return int
      */
-    public function getConnection()
+    public function count($dbName, $addSQL = '', $bindings = array())
     {
-        return $this->toolBox;
+        return Facade::count($dbName, $addSQL, $bindings);
+    }
+
+    /**
+     * @param StoreType $type
+     *
+     * @return int
+     */
+    public function store(StoreType $type)
+    {
+        return $type->storeRedBean();
+    }
+
+    /**
+     * @param AddColumnType $type
+     *
+     * @return array|OODBBean
+     */
+    public function addColumn(AddColumnType $type)
+    {
+        return $type->addRedBeanColumn();
+    }
+
+    /**
+     * @param string $sql
+     * @param array  $bindings
+     *
+     * @return array
+     */
+    public function getAll($sql, $bindings)
+    {
+        Facade::getAll($sql, $bindings);
     }
 
     /**
      * @param string $dbName
+     * @param int    $id
      *
-     * @return int
+     * @return OODBBean
      */
-    public function count($dbName)
+    public function getById($dbName, $id)
     {
-        return Facade::count($dbName);
+        Facade::load($dbName, $id);
     }
 
     /**
-     * @param array|string $stringOrArray
-     *
-     * @return array|OODBBean
+     * @param mixed $column
+     * @param int  $id
      */
-    public function addColumn($stringOrArray)
+    public function delete($column, $id = null)
     {
-        return Facade::dispense($stringOrArray);
+        Facade::trash($column, $id = null);
     }
 
+    /**
+     * @param array $columns
+     */
+    public function deleteAll($columns)
+    {
+        Facade::trashAll($columns);
+    }
+
+    /**
+     * @param string $type
+     * @param string   $sql
+     * @param array  $bindings
+     *
+     * @return array
+     */
+    public function find($type, $sql = null, $bindings = array())
+    {
+        return Facade::find($type, $sql = null, $bindings = array());
+    }
 }
