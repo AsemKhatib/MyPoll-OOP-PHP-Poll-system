6727a67be67d6f280e2bdec43512aca6461baca5
AsemKhatib <ebncana@hotmail.com>
2017-05-24 23:36:54 +0200

    add custom exception class

---
+++ classes/appexception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MyPoll\Classes\Login;
+
+use Exception;
+
+/**
+ * Class AppException
+ * @package MyPoll\Classes\Login
+ */
+class AppException extends Exception
+{
+//TODO : link all exceptions to this one and log them to system.log
+}
