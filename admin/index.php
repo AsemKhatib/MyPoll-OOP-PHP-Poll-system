d48ac78456fb35e0817bc0d83e190e466ff5b438
AsemKhatib <ebncana@hotmail.com>
2017-05-26 21:35:21 +0200

    fix the main routing mechanism in the application

--- admin/index.php
+++ admin/index.php
@@ -13,14 +13,11 @@ $router = isset($_GET['route']) ? General::cleanInput('string', $_GET['route']) 
 
 $adminIndex = $container->get(AdminIndex::class);
 
-//TODO : fix the code here because it's not beautiful nor cool
+$routerInstance = (array_key_exists($router, $container->get('routerArray'))) ? $container->get('routerArray')[$router] : null;
 
 if (!method_exists($adminIndex, $doAction)) {
     $adminIndex->defaultAction();
-} else {
-    if (array_key_exists($router, $container->get('routerArray'))) {
-        $adminIndex->$doAction($container->get('routerArray')[$router]);
-    } else {
-        $adminIndex->$doAction();
-    }
+    exit();
 }
+
+$adminIndex->$doAction($routerInstance);
